d56a67f3c6c503dac9b4bc9d64f60530be2abb4e
etiennecollin <collin.etienne.contact@gmail.com>
2025-08-13 22:13:59 +0200

    feat: improvement of test tab

--- frontend/src/components/tabs/TestTab.tsx
+++ frontend/src/components/tabs/TestTab.tsx
@@ -2,6 +2,7 @@
 
 import { notify } from "@/utils/notifications";
 import Spinner from "@/components/utils/Spinner";
+import VoucherCard from "../VoucherCard";
 
 export default function TestTab() {
   const sendInfo = () => notify("This is an info notification", "info");
@@ -37,6 +38,52 @@ export default function TestTab() {
       <div className="card max-w-lg">
         <h2 className="text-lg font-semibold text-primary">Spinner Example</h2>
         <Spinner />
+      </div>
+
+      <div className="flex-center flex-row gap-4 w-full">
+        <VoucherCard
+          key={123}
+          voucher={{
+            id: "abc123",
+            createdAt: "2025-12-31",
+            name: "test voucher",
+            code: "1234567890",
+            authorizedGuestCount: 0,
+            expired: false,
+            timeLimitMinutes: 1440,
+          }}
+          editMode={false}
+          selected={false}
+        />
+        <VoucherCard
+          key={456}
+          voucher={{
+            id: "abc123",
+            createdAt: "2025-12-31",
+            name: "test voucher",
+            code: "1234567890",
+            authorizedGuestCount: 0,
+            expired: false,
+            timeLimitMinutes: 1440,
+          }}
+          editMode={true}
+          selected={true}
+        />
+        <VoucherCard
+          key={789}
+          voucher={{
+            id: "abc123",
+            createdAt: "2025-12-31",
+            name: "test voucher",
+            code: "1234567890",
+            authorizedGuestCount: 1,
+            expired: true,
+            timeLimitMinutes: 1440,
+            expiresAt: "2025-12-31",
+          }}
+          editMode={true}
+          selected={false}
+        />
       </div>
     </div>
   );
